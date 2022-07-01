575a179525a1ff4fcb312670b1083f0b2e4d784b
Emilio Gonzales <emiliog1184@gmail.com>
2022-07-01 09:49:18 -0700

    add image to menu

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 function Item(props) {
 
   return (
     <React.Fragment>
-      <h3>{props.name} - {props.brewery}</h3>
+      <h3>{props.name} - {props.style}</h3>
       <p>{props.price} - <em>{props.abv}</em></p>
     </React.Fragment>
   );
+}
+
+Item.propTypes = {
+  name: PropTypes.string,
+  style: PropTypes.string,
+  price: PropTypes.number,
+  abv: PropTypes.number
 }
 
 export default Item;
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,16 +1,40 @@
 import React from "react";
 import Item from "../Item/Item";
+import biergarten from './../img/biergarten.jpeg'
+
+const mainMenuList = [
+  {
+    name: 'Pilsner',
+    style: 'German',
+    price: '5',
+    abv: '4.5'
+  },
+  {
+    name: 'Helles',
+    style: 'Munich',
+    price: '4',
+    abv: '4.5'
+  },
+  {
+    name: 'Pilsener',
+    style: 'Czech',
+    price: '6',
+    abv: '5'
+  }
+]
 
 function Menu() {
   return (
     <React.Fragment>
+      <img src={biergarten} alt='An alpine bierhaus' />
       <h1>Haus Menu</h1>
-      <Item
-        name='Pilsner'
-        brewery='Ayinger'
-        price='5'
-        abv='4.5'
-      />
+      {mainMenuList.map((item, index) => 
+        <Item name={item.name}
+          style={item.style}
+          price={item.price}
+          abv={item.abv}
+          key={index} />
+      )}
     </React.Fragment>
   );
 }
